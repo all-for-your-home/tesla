de6332cb8fe4b80cc37fa8ca2e824e4ab1521074
Zayniddin <rikhsiboyevzaxa@gmail.com>
2024-03-12 12:44:09 +0500

    +

---
+++ src/main/java/com/example/allforyourhome/AllForYourHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.allforyourhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllForYourHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllForYourHomeApplication.class, args);
+    }
+
+}
