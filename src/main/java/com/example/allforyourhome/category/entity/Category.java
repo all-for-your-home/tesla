c3aea9f4654fedf5689c4a97bba73612c6c5745d
took4a <zafarmalikov704@gmail.com>
2024-03-16 10:52:52 +0500

    Product va categorylani  crudi tugadi

---
+++ src/main/java/com/example/allforyourhome/category/entity/Category.java
@@ -0,0 +1,32 @@
+package com.example.allforyourhome.category.entity;
+
+import com.example.allforyourhome.product.entity.Product;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL,orphanRemoval = true)
+    @JsonManagedReference
+    private List<Product> products;
+
+}
---
+++ src/main/java/com/example/allforyourhome/product/entity/Product.java
@@ -0,0 +1,38 @@
+package com.example.allforyourhome.product.entity;
+
+import com.example.allforyourhome.category.entity.Category;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String status;
+    private Double price;
+    private String photo;
+    private String color;
+    private String description;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    @JsonBackReference
+    private Category category;
+
+
+}
